835ea21cd37a982e0c7276c4139bd27ff70cc774
Liam Moat <liam.moat@microsoft.com>
2019-05-17 16:54:20 +0100

    feat(dep-injection): example of DI using Autofac

---
+++ src/ASPNET.Fundamentals.Autofac/Repositories/CharacterRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace ASPNET.Fundamentals.Autofac.Repositories
+{
+    public class CharacterRepository : ICharacterRepository
+    {
+        public Task<string> GetCharacterName()
+        {
+            return Task.FromResult("Iron Man");
+        }
+    }
+}
