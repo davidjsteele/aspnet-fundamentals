66cd4f1b8609be298db2be76847411285396cc74
Liam Moat <liam.moat@microsoft.com>
2019-05-19 13:59:27 +0100

    feat(static-files): example project serving static content

---
+++ src/StaticFiles/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using ASPNET.Fundamentals.StaticFiles.Models;
+using Microsoft.AspNetCore.Authorization;
+using System.IO;
+
+namespace ASPNET.Fundamentals.StaticFiles.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult AuthorizedImage()
+        {
+            var file = Path.Combine(Directory.GetCurrentDirectory(),
+                                    "MyStaticFiles", "images", "microsoft.png");
+
+            return PhysicalFile(file, "image/svg+xml");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
